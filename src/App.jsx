70551b50bfefaa9c7e8d0b00802ae405a76d09f3
MOHD <aakil.amity@gmail.com>
2025-03-03 06:47:03 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-// import Newsapp from './Components/Newsapp'
-import Newsapp2nd from './Components/Newsapp2nd'
+import Newsapp from './Components/Newsapp'
 
 const App = () => {
   return (
     <div>
-      {/* <Newsapp /> */}
-      <Newsapp2nd />
+      <Newsapp />
     </div>
   )
 }
